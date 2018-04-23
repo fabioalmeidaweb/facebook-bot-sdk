0a3426e63fb7078cfd9591d670bb3d56d8c54a15
Fabio Almeida <fabio.almeida@hogarthww.com>
2018-04-23 08:24:58 -0300

    Fix EOL and other things.

--- src/CallSendApi.php
+++ src/CallSendApi.php
@@ -6,22 +6,23 @@ use GuzzleHttp\Client;
 class CallSendApi
 {
     const URL = 'https://graph.facebook.com/v2.6/me/messages';
+    const URL_PROFILE = 'https://graph.facebook.com/v2.6/me/messenger_profile';
     private $pageAccessToken;
 
-    public function __construct(string $pageAccessToken) {
+    public function __construct(string $pageAccessToken)
+    {
         $this->pageAccessToken = $pageAccessToken;
     }
 
-    public function make(array $message, string $url = null, $method = 'POST')
+    public function make(array $message, string $url = null, $method = 'POST') :string
     {
         $client = new Client;
         $url = $url ?? CallSendApi::URL;
-
         $response = $client->request($method, $url, [
             'json' => $message,
             'query' => ['access_token' => $this->pageAccessToken]
         ]);
 
-        return (string) $response->getBody();
+        return (string)$response->getBody();
     }
 }
--- src/SenderRequest.php
+++ src/SenderRequest.php
@@ -1,27 +1,22 @@
 <?php
-
 namespace CodeBot;
-
 class SenderRequest
 {
     private $event;
-
-    public function __construct() {
-        $event = file_get_contents('php://input');
+    public function __construct()
+    {
+        $event = file_get_contents("php://input");
         $event = json_decode($event, true, 512, JSON_BIGINT_AS_STRING);
         $this->event = $event['entry'][0]['messaging'][0];
     }
-
     public function getSenderId()
     {
         return $this->event['sender']['id'] ?? null;
     }
-
     public function getMessage()
     {
         return $this->event['message']['text'] ?? null;
     }
-
     public function getPostback()
     {
         if (empty($this->event['postback'])) {
