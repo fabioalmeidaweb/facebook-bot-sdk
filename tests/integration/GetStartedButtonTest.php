b6d53c59a29e32a19a71e4c8591fb3a86bdd19c8
Fabio Almeida <fabio.almeida@hogarthww.com>
2018-07-02 11:16:09 -0300

    Button remove GetStarted

--- tests/integration/GetStartedButtonTest.php
+++ tests/integration/GetStartedButtonTest.php
@@ -9,8 +9,17 @@ class GetStartedButtonTest extends TestCase
     public function testAddGetStartedButton()
     {
         $data = (new GetStartedButton())->add('Iniciar');
-        $callSendApi = new CallSendApi('EAACjHqHVOAMBAOZCZBMhYjtLRcYCAcosIm4q0yjhiFxce1CbVKyc952PB79NQ8w0YVl9BCVypN8qR8SsmewDHCrJGg5OudelTFH4Sg6neWYacPbhgPqlIlpyqPd7QXlXvwIYsdf92zZB5a5hKdvhgd5vijn5ge4yE5aiX6IXjFZA6AVqTKWX');
+        $callSendApi = new CallSendApi('');
         $result = $callSendApi->make($data, CallSendApi::URL_PROFILE);
+
+        $this->assertTrue(is_string($result));
+    }
+
+    public function testRemoveGetStartedButton()
+    {
+        $data = (new GetStartedButton())->remove();
+        $callSendApi = new CallSendApi('');
+        $result = $callSendApi->make($data, CallSendApi::URL_PROFILE, 'DELETE');
 
         $this->assertTrue(is_string($result));
     }
